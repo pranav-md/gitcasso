81204fd6fc22ed8228b2ee42b45d14ce5aa21710
pranav-md <pranavmadavadev78@gmail.com>
2021-11-08 19:09:19 +0530

    Using shelljs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const simpleGit = require('simple-git');
+const shell = require('shelljs');
 
 async function checkChanges() {
     console.log('Entering check changes');
@@ -16,4 +17,60 @@ async function checkChanges() {
     return checked;
    }
 
-checkChanges()
+
+
+   async function branchCheckout(branchName) {
+
+    console.log(`changing branch to ${branchName}`);
+    const git = simpleGit('/home/pranav/sandbox/PP/pp-migration');
+    await git.checkout(branchName);
+   }
+
+
+
+   async function pullLatest(branchName) {
+
+
+//     console.log(`pulling latest of ${branchName}`);
+//     const git = simpleGit('/home/pranav/sandbox/PP/pp-migration');
+
+//  //   await git.addRemote('origin', 'git@github.com:PrivacyAgentHolding/pp-migration.git', )
+//     await git.pull('origin',branchName);
+
+
+    shell.cd('/home/pranav/sandbox/PP/pp-migration')
+
+    shell.exec("git remote set-url origin git@github.com:PrivacyAgentHolding/pp-migration.git", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+    
+        console.log(`stdout: ${stdout}`);
+});
+
+
+    shell.exec("git pull origin", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+    
+        console.log(`stdout: ${stdout}`);
+});
+
+   }
+
+//checkChanges()
+
+//branchCheckout('develop')
+
+pullLatest('develop')
